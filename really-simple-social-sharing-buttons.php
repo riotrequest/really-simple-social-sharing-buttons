d9674c477dfb6728d07c9b88937d3592263f8326
riotrequest <121694664+riotrequest@users.noreply.github.com>
2024-06-20 11:14:24 -0400

    Update really-simple-social-sharing-buttons.php
    Improvements Made:
    Security: Added nonce verification for saving settings.
    URL Generation: Ensured URLs are generated dynamically when rendering the share buttons.
    User Experience: Added a more user-friendly interface with labels and descriptions.
    Enqueueing Styles: Used versioning for the enqueued styles to avoid caching issues.

--- really-simple-social-sharing-buttons.php
+++ really-simple-social-sharing-buttons.php
@@ -2,20 +2,20 @@
 /**
  * Plugin Name: Social Sharing Buttons
  * Description: Inserts share buttons with Font Awesome icons and text for selected social media sites at the top and bottom of the content.
- * Version: 2.0
+ * Version: 2.1
  * Author: RiotRequest
  * License: GPL2
  */
 
 // Enqueue Font Awesome stylesheet
 function enqueue_font_awesome() {
-    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css', array(), '5.15.4');
+    wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css', array(), '6.4.0');
 }
 add_action('wp_enqueue_scripts', 'enqueue_font_awesome');
 
 // Enqueue plugin CSS styles
 function enqueue_social_sharing_styles() {
-    wp_enqueue_style('social-sharing-styles', plugins_url('social-sharing-styles.css', __FILE__));
+    wp_enqueue_style('social-sharing-styles', plugins_url('social-sharing-styles.css', __FILE__), array(), '2.1');
 }
 add_action('wp_enqueue_scripts', 'enqueue_social_sharing_styles');
 
@@ -36,6 +36,8 @@ function social_sharing_options() {
     
     // Check if form is submitted
     if (isset($_POST['submit'])) {
+        check_admin_referer('social_sharing_save_settings', 'social_sharing_nonce');
+        
         // Save the selected social networks configuration
         $selected_networks = isset($_POST['networks']) ? $_POST['networks'] : array();
         update_option('social_sharing_networks', $selected_networks);
@@ -48,6 +50,8 @@ function social_sharing_options() {
         <h1>Social Sharing Settings</h1>
         <form method="post" action="">
             <?php
+            wp_nonce_field('social_sharing_save_settings', 'social_sharing_nonce');
+            
             // Retrieve the available social networks
             $social_sites = get_social_sites();
             
@@ -67,66 +71,56 @@ function social_sharing_options() {
 function get_social_sites() {
     $social_sites = array(
         'Facebook' => array(
-            'url' => 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode(get_permalink()),
             'icon' => 'fa-brands fa-facebook-f',
             'text' => 'Facebook',
             'color' => '#3b5998'
         ),
         'Twitter' => array(
-            'url' => 'https://twitter.com/share?url=' . urlencode(get_permalink()) . '&text=' . urlencode(get_the_title()),
             'icon' => 'fa-brands fa-twitter',
             'text' => 'Twitter',
             'color' => '#1da1f2'
         ),
         'LinkedIn' => array(
-            'url' => 'https://www.linkedin.com/shareArticle?url=' . urlencode(get_permalink()) . '&title=' . urlencode(get_the_title()),
             'icon' => 'fa-brands fa-linkedin',
             'text' => 'LinkedIn',
             'color' => '#0077b5'
         ),
         'Pinterest' => array(
-            'url' => 'https://pinterest.com/pin/create/button/?url=' . urlencode(get_permalink()) . '&media=' . wp_get_attachment_url(get_post_thumbnail_id()) . '&description=' . urlencode(get_the_title()),
             'icon' => 'fa-brands fa-pinterest',
             'text' => 'Pinterest',
             'color' => '#bd081c'
         ),
         'Reddit' => array(
-            'url' => 'https://www.reddit.com/submit?url=' . urlencode(get_permalink()) . '&title=' . urlencode(get_the_title()),
             'icon' => 'fa-brands fa-reddit',
             'text' => 'Reddit',
             'color' => '#ff4500'
         ),
         'Telegram' => array(
-            'url' => 'https://telegram.me/share/url?url=' . urlencode(get_permalink()) . '&text=' . urlencode(get_the_title()),
             'icon' => 'fa-brands fa-telegram',
             'text' => 'Telegram',
             'color' => '#0088cc'
         ),
         'Gab' => array(
-            'url' => 'https://gab.com/compose?url=' . urlencode(get_permalink()),
             'icon' => 'fa-solid fa-frog',
             'text' => 'Gab',
             'color' => '#21cf7a'
         ),
         'Gettr' => array(
-            'url' => 'https://gettr.com/share?text=' . urlencode(get_the_title()) . '&url=' . urlencode(get_permalink()),
             'icon' => 'fa-solid fa-fire-flame-curved',
             'text' => 'Gettr',
             'color' => '#FC223B'
         ),
         'TruthSocial' => array(
-            'url' => 'https://truthsocial.com/share?url=' . urlencode(get_permalink()),
             'icon' => 'fa-solid fa-t',
             'text' => 'Truth Social',
             'color' => '#1f1657'
         ),
         'Email' => array(
-            'url' => 'mailto:?subject=' . urlencode(get_the_title()) . '&body=' . urlencode(get_permalink()),
             'icon' => 'fa fa-envelope',
             'text' => 'Email',
             'color' => '#888888'
         ),
-        // Add more social media sites and their URLs/icons/text/colors as needed
+        // Add more social media sites and their icons/text/colors as needed
     );
     
     return $social_sites;
@@ -142,7 +136,7 @@ function add_social_share_buttons($content) {
         
         foreach ($social_sites as $site => $data) {
             if (in_array($site, $social_networks)) {
-                $url = $data['url'];
+                $url = get_social_share_url($site);
                 $icon = $data['icon'];
                 $text = $data['text'];
                 // Convert the site name to lowercase, remove spaces and prepend "share-"
@@ -159,3 +153,32 @@ function add_social_share_buttons($content) {
     return $content;
 }
 add_filter('the_content', 'add_social_share_buttons');
+
+// Generate the share URL dynamically
+function get_social_share_url($site) {
+    switch ($site) {
+        case 'Facebook':
+            return 'https://www.facebook.com/sharer/sharer.php?u=' . urlencode(get_permalink());
+        case 'Twitter':
+            return 'https://twitter.com/share?url=' . urlencode(get_permalink()) . '&text=' . urlencode(get_the_title());
+        case 'LinkedIn':
+            return 'https://www.linkedin.com/shareArticle?url=' . urlencode(get_permalink()) . '&title=' . urlencode(get_the_title());
+        case 'Pinterest':
+            return 'https://pinterest.com/pin/create/button/?url=' . urlencode(get_permalink()) . '&media=' . wp_get_attachment_url(get_post_thumbnail_id()) . '&description=' . urlencode(get_the_title());
+        case 'Reddit':
+            return 'https://www.reddit.com/submit?url=' . urlencode(get_permalink()) . '&title=' . urlencode(get_the_title());
+        case 'Telegram':
+            return 'https://telegram.me/share/url?url=' . urlencode(get_permalink()) . '&text=' . urlencode(get_the_title());
+        case 'Gab':
+            return 'https://gab.com/compose?url=' . urlencode(get_permalink());
+        case 'Gettr':
+            return 'https://gettr.com/share?text=' . urlencode(get_the_title()) . '&url=' . urlencode(get_permalink());
+        case 'TruthSocial':
+            return 'https://truthsocial.com/share?url=' . urlencode(get_permalink());
+        case 'Email':
+            return 'mailto:?subject=' . urlencode(get_the_title()) . '&body=' . urlencode(get_permalink());
+        default:
+            return '';
+    }
+}
+?>
